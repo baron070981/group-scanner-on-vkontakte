8a6d8e73bbf306520da5fa70daff98babbd378ed
baron070981 <baron070981@gmail.com>
2020-02-02 00:44:20 +0300

    create api

--- datavk.py
+++ datavk.py
@@ -2,9 +2,91 @@ import os
 import sys
 import vk_requests
 
+# 7211649
+
+
+class VkData:
+    def __init__(self):
+        self.__login = ''
+        self.__password = ''
+        self.app_id = 0
+        self.owner_id = 0
+        self.INIT_APP_DATA = False
+        self.INIT_API = False
+        self.GROUP_DATA = False
+        self.api = None
+        self.urls = list()
+        self.cash_id = list()
+        
+    
+    def init_app_data(self, login:str, password:str, app_id:int, owner_id:int):
+        self.__login = login
+        self.__password = password
+        self.app_id = app_id
+        if owner_id == 0:
+            return
+        elif owner_id > 0:
+            self.owner_id = -owner_id
+            self.INIT_APP_DATA = True
+            return
+        else:
+            self.owner_id = owner_id
+            self.INIT_APP_DATA = True
+            return
+    
+    
+    def init_api(self):
+        if self.INIT_APP_DATA == False:
+            self.INIT_API = False
+            return
+        try:
+            self.api = vk_requests.create_api(app_id = self.app_id,
+                                              login = self.__login,
+                                              password = self.__password)
+            self.INIT_API = True
+        except:
+            print('Error init API')
+            self.INIT_API = False
+    
+    
+    def get_data_from_group(self, offset = 0, count =  5):
+        if self.INIT_API == False:
+            self.GROUP_DATA = False
+            return
+        data = self.api.wall.get(owner_id=self.owner_id, offset = offset, count = count)
+        if len(data['items']) > 0:
+            self.GROUP_DATA = True
+            self.urls.clear()
+            for item_data in data['items']:
+                print(item_data)
+                print('\n')
+        else:
+            self.GROUP_DATA = False
 
 
 
 
+
+
+
+if __name__ == '__main__':
+    
+    vk = VkData()
+    print('Start', vk.INIT_APP_DATA, vk.INIT_API)
+    
+    vk.init_app_data('89992948531', 'baron070981', 7211649, -10467310)
+    print('Start', vk.INIT_APP_DATA, vk.INIT_API)
+    
+    vk.init_api()
+    print('Start', vk.INIT_APP_DATA, vk.INIT_API)
+    
+    
+    vk.get_data_from_group()
+    
+    
+    
+    
+    
+    
 
 
